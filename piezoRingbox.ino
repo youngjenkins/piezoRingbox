3079160af15427c8a60209f5d1edd1408e5577f5
youngjenkins <dhoch984@gmail.com>
2017-08-05 16:07:21 -0400

    Add files via upload

--- piezoRingbox.ino
+++ piezoRingbox.ino
@@ -52,28 +52,27 @@ void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMillisecon
 
 
 void beepSong(char buzzPin){
-  int arrayLengths = 32;
+	int arrayLengths = 32;
 
-//frequencies
-int fq[] = {1319, 1480, 1661, 1760, 1661, 1480, 1319, 1661,
- 1480, 1319, 1175, 1480, 1319, 1175, 1109, 1319, 0, 1109, 988,
- 880, 988, 1109, 1175, 988, 1109, 1175, 0, 1319, 1319, 1480,
- 1661, 1480};
+		/frequencies
+	int fq[] = {1319, 1480, 1661, 1760, 1661, 1480, 1319, 1661,
+	 1480, 1319, 1175, 1480, 1319, 1175, 1109, 1319, 0, 1109, 988,
+	 880, 988, 1109, 1175, 988, 1109, 1175, 0, 1319, 1319, 1480,
+	 1661, 1480};
 
-//durations
-int duration[] = {226, 226, 226, 226, 226, 339, 113, 226,
- 226, 339, 113, 226, 226, 113, 226, 452, 226, 113, 113, 226,
- 226, 226, 226, 113, 226, 339, 113, 113, 226, 226, 113, 452};
-  
-  for(int i=0; i<arrayLengths; i++){
-    if(fq[i]==0){          // when pitch==0 pause prgm
-      delay(duration[i]);  // unless you want to divide by 0
-    }
-    else{
-      beep(buzzPin, fq[i], duration[i]);
-    }
-      
-  }
+		//durations
+	int duration[] = {226, 226, 226, 226, 226, 339, 113, 226,
+	 226, 339, 113, 226, 226, 113, 226, 452, 226, 113, 113, 226,
+	 226, 226, 226, 113, 226, 339, 113, 113, 226, 226, 113, 452};
+	  
+	for(int i=0; i<arrayLengths; i++){
+		if(fq[i]==0){            // when pitch==0 pause prgm
+			delay(duration[i]);  // unless you want to divide by 0
+		}
+		else{
+			beep(buzzPin, fq[i], duration[i]);
+		} 
+	  }
 }
 
     
